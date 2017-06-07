877a1068e8c8be7343a987b419742b7e25321be0
lihao <275@op110.com>
2017-06-07 14:38:47 +0800

    'init'

---
+++ yiranlihao-util-entity/src/main/java/per/yiranlihao/util/entity/User.java
@@ -0,0 +1,13 @@
+package per.yiranlihao.util.entity;
+
+import lombok.Data;
+
+/**
+ * Created by Lihao on 2017/6/7.
+ */
+@Data
+public class User {
+    private Integer id;
+    private String name;
+    private Byte sex;
+}
